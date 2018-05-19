b8185a8e0604cc7276d08a7905c6a4d03fe876ae
doorfish <bnguyen16@gmail.com>
2018-05-19 13:23:00 -0500

    added orm code for users and for bmi calc

--- models/user.js
+++ models/user.js
@@ -1,29 +1,29 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-var bmi = {
+var user = {
   all: function(cb) {
-    orm.all("bmi", function(res) {
+    orm.all("user", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
-    orm.create("bmi", cols, vals, function(res) {
+    orm.create("user", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("bmi", objColVals, condition, function(res) {
+    orm.update("user", objColVals, condition, function(res) {
       cb(res);
     });
   },
   delete: function(condition, cb) {
-    orm.delete("bmi", condition, function(res) {
+    orm.delete("user", condition, function(res) {
       cb(res);
     });
   }
 };
 
-// Export the database functions for the controller (bmiController.js).
-module.exports = bmi;
+// Export the database functions for the controller (userController.js).
+module.exports = user;
